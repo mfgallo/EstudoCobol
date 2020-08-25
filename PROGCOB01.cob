b29e69925d7364f4cbf359ac833081cee7242411
Mauricio Gallo <mauriciogallojr1993@gmail.com>
2020-08-24 22:56:46 -0300

    Primeiro commit de estudos de COBOL

---
+++ PROGCOB01.cob
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+
+      *********************************************************************
+      *AREA DE REMARKS
+      *AUTHOR = Mauricio Gallo Fausto Jr
+      *********************************************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO ALURA'.
+           STOP RUN.
